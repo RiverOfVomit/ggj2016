2058458768519f7da66622bca17a8fd3c5aff8f4
vagrant <vagrant@rov-gg2016>
2016-01-29 22:05:38 +0000

    echo event

--- rov-ggj2016/main.py
+++ rov-ggj2016/main.py
@@ -1,6 +1,6 @@
 
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
 
@@ -16,19 +16,30 @@ def index():
     #return "Hello World!"
     return render_template('index.html')
 
+
 @app.route("/client")
 def client():
     #return "Hello World!"
     return render_template('client.html')
+
 
 @app.route("/board")
 def board():
 	#return "Board is up!"
 	return renter_template("board.html")
 
+
 @socketio.on('my event')
 def handle_my_custom_event(json):
     print('received json: ' + str(json))
+
+
+@socketio.on("echo")
+def handle_my_custom_event(string):
+	emit("This is echo alpha seven", string)
+	print "Echo sent"
+	# All clients will messages from emit().
+
 
 if __name__ == "__main__":
     #app.run('',8080,debug=True)

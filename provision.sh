a70bdb5d2ffcdd2112ae3fe1fd00388e3eb078ef
Sebastian Patino-Lang <patino@p2lab.de>
2016-01-29 21:05:49 +0100

    ggj2016 kickoff commit

--- provision.sh
+++ provision.sh
@@ -11,6 +11,10 @@ apt-get -y install curl
 #apt-get -y install npm
 apt-get -y install rubygems
 
+# http://stackoverflow.com/questions/11094718/error-command-gcc-failed-with-exit-status-1-while-installing-eventlet
+apt-get -y install python-dev
+apt-get -y install libevent-dev
+
 #npm install -g node-inspector
 
 npm install -g bower
@@ -37,7 +41,6 @@ pip install flask-socketio
 # http://virtualenvwrapper.readthedocs.org/en/latest/index.html
 # You will want to add the command to source /usr/local/bin/virtualenvwrapper.sh to your shell startup file
 
-gem install rhc 
+gem install rhc
 
 git clone https://github.com/photonstorm/phaser/tree/v2.4.4
-
--- rov-ggj2016/main.py
+++ rov-ggj2016/main.py
@@ -1,12 +1,30 @@
+
 from flask import Flask, render_template
+from flask_socketio import SocketIO
+
 app = Flask(__name__)
 
 print("starting up server")
 
+app.config['SECRET_KEY'] = 'secret!'
+app.config['DEBUG'] = True
+
+socketio = SocketIO(app)
+
 @app.route("/")
-def hello():
+def index():
     #return "Hello World!"
     return render_template('index.html')
 
+@app.route("/client")
+def client():
+    #return "Hello World!"
+    return render_template('client.html')
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
+
 if __name__ == "__main__":
-    app.run('',8080,debug=True)
+    #app.run('',8080,debug=True)
+    socketio.run(app,'0.0.0.0',8080)

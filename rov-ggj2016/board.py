f3c49823daf74a5d90e41a650af5323c4af66cd8
jlee7 <joseflee7@gmail.com>
2016-01-30 00:40:41 +0100

    board and tiles

--- rov-ggj2016/board.py
+++ rov-ggj2016/board.py
@@ -1,5 +1,33 @@
 """Board"""
+from tiles import *
 
 class Board(object):
 	def __init__(self):
+		self.config = (3,5)
+		self.tiles = {}
+
+	def initalize_tiles(self, config):
+		"""Creates all tiles and puts them into a dictionary"""
+		print "x:", config[0], "y:", config[1]
+
+		# VERSION ONE
+		for x in range(config[0]):
+			for y in range(config[1]):
+				self.tiles[x,y] = Tile((x,y))
+				# TEST
+				print self.tiles[(x,y)].id
+
+		print len(self.tiles)
+
+	def send_to_board(self, config):
+		"""Send config to board.html, so tiles will be generated"""
 		pass
+
+
+	def update_tiles(self):
+		pass
+
+
+tc = Board()
+tc.initalize_tiles(tc.config)
+print tc.tiles
--- rov-ggj2016/tiles.py
+++ rov-ggj2016/tiles.py
@@ -1,5 +1,17 @@
 """Tiles"""
 
 class Tile(object):
-	def __init__(self):
-		pass
+	def __init__(self, coordinate):
+		self.states = ["open", "reserved", "solved"]
+		self.id = coordinate
+		self.state = "open"
+		self.player = ""
+
+	def change_state(self, state):
+		if state in self.states:
+			self.state = state
+		else:
+			print "wrong state given"
+
+	def set_player(self, player_name):
+		self.player = player_name

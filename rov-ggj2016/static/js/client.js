f0acf3775c2456660c6666542be8b5b74bfd3b01
M-Kuenstler <manuel_kuenstler@web.de>
2016-01-30 22:34:40 +0100

    client calls

--- rov-ggj2016/static/js/client.js
+++ rov-ggj2016/static/js/client.js
@@ -2,38 +2,55 @@
 	CLIENT
 -----------------------------------------------*/
 var loadingSpinner = $('.loading'),
-    topBbar = $('.top-bar'),
+    topBar = $('.top-bar'),
+    game = $('.mini-game-inner'),
+    gameBtn = $('.mini-game-btn'),
     socket = io.connect('http://' + document.domain + ':' + location.port + '/client');
 
 var spinnerIn = function() {
-    loadingSpinner.fadeIn();
-    topBbar.animate({top: "-190px"}, 500);
-    $('.mini-game-inner ').delay('200').fadeIn('300');
+    loadingSpinner.fadeIn();    
 };
 
 var spinnerOut = function() {
-    $('.mini-game-inner ').delay('200').fadeOut('300');
-    topBbar.animate({top: "0"}, 500);
     loadingSpinner.fadeOut();
 };
+
+var topBarIn = function() {
+    topBar.animate({top: "0"}, 500);    
+};
+
+var topBarOut = function() {
+    topBar.animate({top: "-160px"}, 500);
+
+  };
+
+var gameIn = function() {
+    game.fadeIn('300');
+};
+
+var gameOut = function() {
+    game.fadeOut('300');
+};
+
 
 spinnerIn(); // during initial load until player create event is returned
 
 socket.on('connect', function() {
     $(".mini-game-btn").click(function() {
-      socket.emit('echo', {data: 'Echo-String-on-click'});
       socket.emit('button pushed', {data: 'Button pushed!'});
     });
 });
 
-socket.on('choose tile result', function(msg) {
-	console.log(msg); //player object || false
-	spinnerOut();
-});
-
+// create player
 socket.on('player create result', function(msg) {
-	console.log(msg); //player object || false
-	spinnerOut();
+  console.log(msg); //player object || false
+  if (msg === false) {
+    alert('maximale Spieleranzahl erreicht');
+  }else{
+    var msgOb = jQuery.parseJSON(msg);
+    $('body').addClass('player-' + msgOb.type);
+    spinnerOut();
+  };  
 });
 
 // choose tile
@@ -51,6 +68,23 @@ $('#choose-tile-form').submit(function(){
   }
   return false;
 });
+
+// choose tile response
+socket.on('choose tile result', function(msg) {
+	//player object || false
+	spinnerOut();
+  topBarOut();
+  gameIn();
+});
+
+// resolve tile
+gameBtn.click(function(){
+  socket.emit('resolve tile', { "gameResolved": true });
+  gameOut();
+  topBarIn();
+  $('#choose-tile-form input').val('');
+});
+
 
 // create counter
 // function counter(){

1b6db3e35ea6d4671097919e56fbac9fe44d9048
unknown <manuel_kuenstler@web.de>
2016-01-30 02:45:31 +0100

    choose tile on client

--- rov-ggj2016/static/js/client.js
+++ rov-ggj2016/static/js/client.js
@@ -16,7 +16,36 @@ var socket = io.connect('http://' + document.domain + ':' + location.port);
     //     socket.emit('echo', {data: 'Echo-String-on-connect'});
     // });
 
+// choose tile
+$('#choose-tile-form').submit(function(){
+  var value = $(this).find('input').val();
+  console.log(value);
+
+  if (!value == '') {
+    socket.emit('choose tile', { "tile": value });
+    $(this).fadeOut('300');
+    $('#mini-game').delay('200').fadeIn('300');
+    counter();
+  }else{
+    console.log('Bitte Zahl eingeben');
+  };
+  
+
+ 
+  return false;
+});
+
 // create counter
 function counter(){
-	var multiplier //1 5
+	var num = Math.floor(Math.random()*5+1), // number between 1 and 5
+      numEl = $('.counter-num');
+      numEl.text(num);
+
+for (i = num; i < cars.length; i--) { 
+    text += cars[i] + "<br>";
+}
+
+  setTimeout(function(){ 
+    alert("countdown läuft"); }, 1000
+    );
 };

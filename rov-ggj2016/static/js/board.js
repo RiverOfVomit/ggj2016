5c5f3629d6ac460f581759cf21f3b96ddfeaeb58
M-Kuenstler <manuel_kuenstler@web.de>
2016-01-31 12:00:02 +0100

    board style

--- rov-ggj2016/static/js/board.js
+++ rov-ggj2016/static/js/board.js
@@ -23,7 +23,7 @@ socket.on('tiles update', function(msg) {
     	var obj = tiles[i],
     		tileId = obj.id,
     		state = obj.state;
-    	board.find('.tile-' + tileId).addClass(state);
+    	board.find('.tile-' + tileId).addClass(state);        
     });
 });
 
